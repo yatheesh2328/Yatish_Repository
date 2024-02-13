3d7d3516eeb82a7e76527b0888e65225a59f1fef
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-02-13 12:57:00 +0530

    Update sonar.groovy

--- shared_lib/vars/sonar.groovy
+++ shared_lib/vars/sonar.groovy
@@ -1,5 +1,5 @@
 def call() {
   //SonarQube scan steps
-  sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=// -Dsonar.java.binaries=targest/classes'
+  sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=//16.171.24.243:9000 -Dsonar.java.binaries=targest/classes'
 }
 return this
