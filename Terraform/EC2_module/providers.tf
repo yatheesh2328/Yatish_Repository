379c5e07defeb8b7433b7dd8759b45b5a8dcb57a
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-03-04 16:48:49 +0530

    Update providers.tf

--- Terraform/EC2_module/providers.tf
+++ Terraform/EC2_module/providers.tf
@@ -1,1 +1,13 @@
-
+provider "aws" {
+    region = "eu-west-2"
+}
+ 
+terraform {
+    backend "s3" {
+      region = "eu-west-2"
+      bucket = "yatheesh2328bucket"
+      key = "state.tfstate"
+      dynamodb_table = "statelock"
+      encrypt = true
+    }
+}
