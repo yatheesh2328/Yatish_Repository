51b4f05875d2f2ac485e781b7ac1df8eaed6fa70
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-03-04 17:10:12 +0530

    Update variables.tf

--- Terraform/EC2_module/variables.tf
+++ Terraform/EC2_module/variables.tf
@@ -1,10 +1,10 @@
 variable "ami" {
-    default = "ami-0d18e50ca22537278"
+    //default = "ami-0d18e50ca22537278"
     type = string
   
 }
 
 variable "instance_type" {
-    default = "t2.micro"
+    //default = "t2.micro"
     type = string
 }
